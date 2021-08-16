2fab4d5dba8119b0139a42f98778d97565f2edbd
ankitcn-47 <ankit.codenamefourty7@gmail.com>
2021-08-16 09:55:06 +0530

    correction

--- realWebsiteScrapping/scrapper2.py
+++ realWebsiteScrapping/scrapper2.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
-html = requests.get('')
+html = requests.get('https://www.google.com').text
+data = BeautifulSoup(html, 'lxml').prettify()
+print(data)
 
 
